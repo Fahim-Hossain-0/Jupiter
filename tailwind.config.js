09d22217f7d8c8e6e97d3a233d103c4c14ed1493
fahim-hossain-0 <fahimhossain735@gmail.com>
2024-12-16 00:32:20 +0600

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        dropShadow:{
+          '99xl' : '0 25px 50px -30px rgb(0 0 0 / 0.25)'
+        }
+      },
     },
     plugins: [],
   }
